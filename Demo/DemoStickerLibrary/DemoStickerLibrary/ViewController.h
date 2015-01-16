4699f3fea0ecf3e00d6ab7140acdb39c90c10944
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-17 00:41:32 +0800

    just manual add commit code for Commit: ee1f45a08c173b2391df608671425ade5ee29d44 (for test.)

--- Demo/DemoStickerLibrary/DemoStickerLibrary/ViewController.h
+++ Demo/DemoStickerLibrary/DemoStickerLibrary/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  DemoStickerLibrary
 //
-//  Created by Robin Hsu on 2015/1/16.
+//  Created by Robin Hsu on 2015/1/15.
 //  Copyright (c) 2015年 TechD. All rights reserved.
 //
 
