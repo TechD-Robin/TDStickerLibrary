b5681a7160def876c13766906068455da5578f4a
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-17 01:55:54 +0800

    just manual add commit code for Commit: ce86a2f2be16f2ad35a5d50bb1199dc9b0b59212 (for test.)

--- TDStickerLibrary/TDStickerLibrary.h
+++ TDStickerLibrary/TDStickerLibrary.h
@@ -2,12 +2,22 @@
 //  TDStickerLibrary.h
 //  TDStickerLibrary
 //
-//  Created by Robin Hsu on 2015/1/16.
+//  Created by Robin Hsu on 2015/1/15.
 //  Copyright (c) 2015年 TechD. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#ifndef _TDSticker_Library_
+    #define _TDSticker_Library_
+
+    #import "TDStickerLibraryViewController.h"
+
+#endif  //  End of _TDSticker_Library_.
+
 
 @interface TDStickerLibrary : NSObject
 
 @end
+
