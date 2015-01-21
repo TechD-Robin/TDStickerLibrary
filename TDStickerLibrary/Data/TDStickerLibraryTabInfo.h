40de2e5efbc8f66a4abb12e6265a6c2378c7fb4e
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-21 21:43:35 +0800

    add a data class TDStickerLibrary Tab Info.

---
+++ TDStickerLibrary/Data/TDStickerLibraryTabInfo.h
@@ -0,0 +1,20 @@
+//
+//  TDStickerLibraryTabInfo.h
+//  DemoStickerLibrary
+//
+//  Created by Robin Hsu on 2015/1/21.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDStickerLibraryTabInfo : NSObject
+
+
+
++ (instancetype) loadData:(NSString *)filename;
+
+
+@end
+
+
