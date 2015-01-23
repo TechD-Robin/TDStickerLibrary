db2770aac171d4f5d252374a9916f36aa7799562
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-23 17:41:12 +0800

    add some commit for base tab men item.

--- Libraries/TDBaseObjects/TDBaseObjects/TDBaseTabMenuItem.h
+++ Libraries/TDBaseObjects/TDBaseObjects/TDBaseTabMenuItem.h
@@ -16,17 +16,44 @@
 
 //  ------------------------------------------------------------------------------------------------
 //  ------------------------------------------------------------------------------------------------
+/**
+ *  the base tab menu item will group the same class objects, when tap it will show relation view at the same time.
+ */
 @interface TDBaseTabMenuItem : UIImageView
 
 //  ------------------------------------------------------------------------------------------------
 #pragma mark property of variable.
 //  ------------------------------------------------------------------------------------------------
+/**
+ *  assign a view pointer for a relation view.
+ */
 @property ( nonatomic, SAFE_ARC_PROP_RETAIN ) UIView                      * relationView;
 
 //  ------------------------------------------------------------------------------------------------
 #pragma mark declare for create the object.
 //  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief create a base Tab Menu Item  Object with frame & a image.
+ *  create a base Tab Menu Item  Object with frame & a image.
+ *
+ *  @param frame                    item's frame.
+ *  @param image                    a image attach to the item object for show.
+ *
+ *  @return object|nil              the base Tab Menu Item Object or nil.
+ */
 + ( instancetype ) tabMenuItemWithFrame:(CGRect)frame image:(UIImage *)image;
+
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @create a base Tab Menu Item  Object with frame & a image and high lighted image.
+ *  create a base Tab Menu Item  Object with frame & a image and high lighted image.
+ *
+ *  @param frame                    item's frame.
+ *  @param image                    a image attach to the item object for show.
+ *  @param highlightedImage         a image attach to the item object for object set to high lighted.
+ *
+ *  @return object|nil              the base Tab Menu Item Object or nil.
+ */
 + ( instancetype ) tabMenuItemWithFrame:(CGRect)frame image:(UIImage *)image highlightedImage:(UIImage *)highlightedImage;
 
 
