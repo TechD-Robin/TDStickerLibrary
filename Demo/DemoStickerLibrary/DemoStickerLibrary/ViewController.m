ea725ffbdff086e2f87c2b2dd1b595bef0033e4d
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-25 01:07:03 +0800

    add a method for download test. (really test =..=)

--- Demo/DemoStickerLibrary/DemoStickerLibrary/ViewController.m
+++ Demo/DemoStickerLibrary/DemoStickerLibrary/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "UIKit+TechD.h"
 #import "TDStickerLibrary.h"
+#import "AFNetworking.h"
 
 #import "ViewController.h"
 
@@ -88,6 +89,86 @@
 //    NSLog( @"new get status bar height : %f", [[UIScreen mainScreen] getStatusBarHeight] );
 }
 
+
+//  ------------------------------------------------------------------------------------------------
+//  --------------------------------
+- (void) _CreateDownloadButton
+{
+    UIButton                      * button;
+    CGRect                          mainRect;
+    
+    mainRect                        = [[UIScreen mainScreen] bounds];
+    //.NSLog( @"%s", [NSStringFromCGRect( mainRect ) UTF8String] );
+    button                          = [UIButton buttonWithType: UIButtonTypeInfoDark];
+    if ( nil == button )
+    {
+        return;
+    }
+    
+    [button                         setBackgroundColor: [UIColor darkGrayColor]];
+    [button                         setFrame: CGRectMake( 0, 0, 240, 36)];
+    [button                         setCenter: CGPointMake( ( mainRect.size.width / 2.0f ), ( mainRect.size.height /2.0f ) )];
+    [button                         setTitle: @" Download Test " forState: UIControlStateNormal];
+    [[self                          view] addSubview: button];
+    
+    UITapGestureRecognizer        * tap;
+    
+    tap                             = [[UITapGestureRecognizer alloc] initWithTarget: self action: @selector( _TapDownloadAction: )];
+    if ( nil == tap )
+    {
+        return;
+    }
+    [button                         addGestureRecognizer: tap];
+    
+    
+    [button                         setTranslatesAutoresizingMaskIntoConstraints: NO];
+    
+    NSLayoutConstraint            * layoutX;
+    NSLayoutConstraint            * layoutY;
+    layoutX                         = [NSLayoutConstraint constraintWithItem: button attribute: NSLayoutAttributeCenterX relatedBy: NSLayoutRelationEqual
+                                                                      toItem: [self view] attribute:NSLayoutAttributeCenterX  multiplier: 1.0f constant: 0.0f];
+    
+    layoutY                         = [NSLayoutConstraint constraintWithItem: button attribute: NSLayoutAttributeTop relatedBy: NSLayoutRelationEqual
+                                                                      toItem: [self view] attribute: NSLayoutAttributeTopMargin multiplier: 1.0f constant: 80.0f];
+    
+    [[self view] addConstraint: layoutX];
+    [[self view] addConstraint: layoutY];
+    
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( void ) _TapDownloadAction:(id)sender
+{
+    NSLog( @"download test" );
+    
+    NSString                      * link;
+    
+
+//    link                            = @"https://www.dropbox.com/s/p04fqtkggiwpq16/barcode%20cat_240x240_readline.png?dl=1";
+    link                            = @"https://docs.google.com/uc?authuser=0&id=0B1yHM9LysIXXRXVPZnFqYWxIMk0&export=download";
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString: link];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    //.NSProgress *progress = nil;
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress: nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"!!!!!!!!!!!! File downloaded to: %@", filePath);
+    }];
+    [downloadTask resume];
+    
+    
+    
+
+}
+
+
+
 //  ------------------------------------------------------------------------------------------------
 //  --------------------------------
 - (void)viewDidLoad
@@ -98,6 +179,7 @@
 
     
     [self                           _CreateButton];
+    [self                           _CreateDownloadButton];     //  for download update test.
     
 //    [[UIApplication sharedApplication] setStatusBarHidden: NO withAnimation:UIStatusBarAnimationFade];
 }
