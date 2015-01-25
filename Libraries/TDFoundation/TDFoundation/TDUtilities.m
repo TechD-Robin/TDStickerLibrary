e441fdcba0ca8b2f2d2732ac65ea19d86f9c1211
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-25 13:53:57 +0800

    add a library : TDFoundation.

---
+++ Libraries/TDFoundation/TDFoundation/TDUtilities.m
@@ -0,0 +1,110 @@
+//
+//  TDUtilities.m
+//  TDFoundation
+//
+//  Created by Robin Hsu on 2015/1/25.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import "TDUtilities.h"
+
+@implementation TDUtilities
+
+@end
+
+
+//TDLibraryDirectory              = NSLibraryDirectory,
+//TDDocumentDirectory             = NSDocumentDirectory,
+//TDCachesDirectory               = NSCachesDirectory,
+//
+//TDHomeDirectory                 = 201,
+//TDTemporaryDirectory            = 202,
+//
+//TDResourcesDirectory            = 301,
+
+//  --------------------------------
+
+//  ------------------------------------------------------------------------------------------------
+NSString * TDGetPathForDirectories( TDGetPathDirectory directory, NSString * name, NSString * typeExt, NSString * inDirectorySubpath )
+{
+
+    NSArray                       * searchList;
+    NSString                      * searchPath;
+    NSString                      * filename;
+    NSString                      * filenameWithExt;
+    
+    searchList                      = nil;
+    searchPath                      = nil;
+    filename                        = nil;
+    filenameWithExt                 = nil;
+    switch ( directory )
+    {
+        case TDLibraryDirectory:
+        case TDDocumentDirectory:
+        case TDCachesDirectory:
+        {
+            searchList              = NSSearchPathForDirectoriesInDomains( (NSSearchPathDirectory)directory, NSUserDomainMask, YES );
+            searchPath              = [searchList objectAtIndex: 0];
+            break;
+        }
+        case TDHomeDirectory:
+        {
+            searchPath              = NSHomeDirectory();
+            break;
+        }
+        case TDTemporaryDirectory:
+        {
+            searchPath              = [NSTemporaryDirectory() stringByStandardizingPath];
+            break;
+        }
+        case TDResourcesDirectory:
+        {
+            filename                = [[NSBundle mainBundle] pathForResource: name ofType: typeExt inDirectory: inDirectorySubpath];
+            return filename;
+        }
+            
+        default:
+        {
+            return nil;
+        }
+    }
+    
+    if ( ( nil != inDirectorySubpath ) && ( [inDirectorySubpath isEqualToString: @""] == NO ) )
+    {
+        searchPath                  = [searchPath stringByAppendingFormat: @"/%s", [inDirectorySubpath UTF8String]];
+    }
+    filename                        = [searchPath stringByAppendingFormat: @"/%s", [name UTF8String]];
+    
+    
+    if ( ( nil != typeExt ) && ( [typeExt isEqualToString: @""] == NO ) )
+    {
+        filename                    = [filename stringByAppendingFormat: @".%s", [typeExt UTF8String]];
+    }
+
+    if ( [[NSFileManager defaultManager] fileExistsAtPath: filename] == NO )
+    {
+        return nil;
+    }
+
+    return filename;
+}
+
+
+//  ------------------------------------------------------------------------------------------------
+
+
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
