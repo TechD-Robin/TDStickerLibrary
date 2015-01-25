b486271780aea63900beb78fd3d55665b81d1eb6
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-25 22:40:40 +0800

    add a comment for TDFoundation's  TDGetPathForDirectories method.

--- Libraries/TDFoundation/TDFoundation/TDUtilities.h
+++ Libraries/TDFoundation/TDFoundation/TDUtilities.h
@@ -12,22 +12,32 @@
 
 @end
 
-typedef NS_ENUM( NSUInteger, TDGetPathDirectory ) {
+/**
+ *  directory enumeration, and refer from NSSearchPathDirectory to define.
+ */
+typedef NS_ENUM( NSUInteger, TDGetPathDirectory ){
     TDLibraryDirectory              = NSLibraryDirectory,
     TDDocumentDirectory             = NSDocumentDirectory,
     TDCachesDirectory               = NSCachesDirectory,
-    
     TDHomeDirectory                 = 201,
     TDTemporaryDirectory            = 202,
-    
     TDResourcesDirectory            = 301,
-    
 };
 
 
 //  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief get the string of file's full path with path directory's enumeration.
+ *  get the string of file's full path with path directory's enumeration, merge with NSSearchPathForDirectoriesInDomains and NSBundle's pathForResource: ofType: inDirectory: method.
+ *
+ *  @param directory                enumeration of directory.
+ *  @param name                     the filename want to find.
+ *  @param typeExt                  ext port of filename.(sub filename)
+ *  @param inDirectorySubpath       sub path in directory.
+ *
+ *  @return full path|nil           the file's full path or nil.
+ */
 NSString * TDGetPathForDirectories( TDGetPathDirectory directory, NSString * name, NSString * typeExt, NSString * inDirectorySubpath );
-
 
 //  ------------------------------------------------------------------------------------------------
 
