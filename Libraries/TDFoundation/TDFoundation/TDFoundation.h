0aad1fe6d1e0cef66b8942fb5e70160bb8123203
Robin Hsu <robinhsu599+dev@gmail.com>
2015-01-30 02:17:17 +0800

    update library : TDFoundation files & add to project.

---
+++ Libraries/TDFoundation/TDFoundation/TDFoundation.h
@@ -0,0 +1,24 @@
+//
+//  TDFoundation.h
+//  TDFoundation
+//
+//  Created by Robin Hsu on 2015/1/30.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import <Availability.h>
+#import <CoreGraphics/CoreGraphics.h>
+
+#ifndef _TDFoundation_
+    #define _TDFoundation_
+
+    #import "TDUtilities.h"
+
+    #import "TDTexturePakcerXMLReader.h"
+#endif  //  End of _TDFoundation_.
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
