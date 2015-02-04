f7e27cc42957319c405e8c349a1ea2bfb039cc6e
Robin Hsu <robinhsu599+dev@gmail.com>
2015-02-05 00:14:37 +0800

    add collection table object classes.

---
+++ TDStickerLibrary/TDStickerLibrarySectionPreviewCell.h
@@ -0,0 +1,13 @@
+//
+//  TDStickerLibrarySectionPreviewCell.h
+//  TDStickerLibrary
+//
+//  Created by Robin Hsu on 2015/2/4.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDStickerLibrarySectionPreviewCell : UICollectionViewCell
+
+@end
