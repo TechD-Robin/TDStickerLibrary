4b3bcb547d099e9df3ae34b005e79cdb9bacd243
Robin Hsu <robinhsu599+dev@gmail.com>
2015-02-05 00:14:37 +0800

    add collection table object classes.

---
+++ TDStickerLibrary/TDStickerLibrarySectionHeader.m
@@ -0,0 +1,13 @@
+//
+//  TDStickerLibrarySectionHeader.m
+//  TDStickerLibrary
+//
+//  Created by Robin Hsu on 2015/2/4.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import "TDStickerLibrarySectionHeader.h"
+
+@implementation TDStickerLibrarySectionHeader
+
+@end
