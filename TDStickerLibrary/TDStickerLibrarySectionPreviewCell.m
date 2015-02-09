209f6bb3126826e6dd6a363baa76cf6c6c52b1db
Robin Hsu <robinhsu599+dev@gmail.com>
2015-02-09 23:52:42 +0800

    add a method to receive image configure for cell ( preview ). add a tap action for cell that can get a sticker info from preview image( the cell's image of background view ).

--- TDStickerLibrary/TDStickerLibrarySectionPreviewCell.m
+++ TDStickerLibrary/TDStickerLibrarySectionPreviewCell.m
@@ -9,11 +9,13 @@
 //  ------------------------------------------------------------------------------------------------
 
 #ifndef __ARCMacros_H__
-#import "ARCMacros.h"
+    #import "ARCMacros.h"
 #endif  //  End of __ARCMacros_H__.
 
+#import "TDMath.h"
 
 #import "TDStickerLibrarySectionPreviewCell.h"
+#import "TDTexturePakcerXMLReader.h"
 
 //  ------------------------------------------------------------------------------------------------
 //  ------------------------------------------------------------------------------------------------
@@ -31,8 +33,12 @@
 #pragma mark declare property ()
 @interface TDStickerLibrarySectionPreviewCell ()
 {
+    TDTexturePakcerXMLReader      * xmlReader;
+    NSData                        * framesData;
     
 }
+
+//  --------------------------------
 //  ------------------------------------------------------------------------------------------------
 
 @end
@@ -58,6 +64,25 @@
  */
 - ( void ) _InitAttributes;
 
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief create tap gesture recognizer.
+ *  create tap gesture recognizer for self (cell object).
+ *
+ *  @return YES|NO                  method success or failure
+ */
+- ( BOOL ) _CreateTapAction;
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief calculate the scale of size.
+ *  calculate the scale of size that is change for texture(original image) assign to the object's background view.
+ *
+ *  @return value| 0.0f               calculated value for scale or 0.0f.
+ */
+- ( CGFloat ) _CalculateScaleOfSize;
+
 
 //  ------------------------------------------------------------------------------------------------
 
@@ -80,8 +105,122 @@
 //  ------------------------------------------------------------------------------------------------
 - ( void ) _InitAttributes
 {
+    xmlReader                       = nil;
+    framesData                      = nil;
+    
+    
     [self                           setMiniState: YES];
 }
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+- ( BOOL ) _CreateTapAction
+{
+    UITapGestureRecognizer        * tap;
+    
+    tap                             = [[UITapGestureRecognizer alloc] initWithTarget: self action: @selector( _TapAction: )];
+    if ( nil == tap )
+    {
+        return NO;
+    }
+    
+    [self                           addGestureRecognizer: tap];
+    SAFE_ARC_RELEASE( tap );
+    SAFE_ARC_ASSIGN_POINTER_NIL( tap );
+    return YES;
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( void ) _TapAction:(UITapGestureRecognizer *) sender
+{
+    if ( nil == framesData )
+    {
+        return;
+    }
+    
+    if ( nil == xmlReader )
+    {
+        xmlReader                   = [TDTexturePakcerXMLReader loadFromData: framesData];
+    }
+    if ( nil == xmlReader )
+    {
+        return;
+    }
+    
+    CGRect                          stickerFrame;
+    CGRect                          stickerNewFrame;
+    CGPoint                         touchPoint;
+    NSString                      * stickerName;
+    NSString                      * textureName;
+    CGFloat                         scale;
+    
+    stickerFrame                    = CGRectZero;
+    stickerNewFrame                 = CGRectZero;
+    touchPoint                      = [sender locationInView: self];
+    scale                           = [self _CalculateScaleOfSize];
+    textureName                     = [xmlReader textureName];
+    if ( ( nil == xmlReader ) || ( [xmlReader spriteDataCount] == 0 ) )
+    {
+        return;
+    }
+    
+    for ( int i = 0; i < [xmlReader spriteDataCount]; ++i )
+    {
+        stickerFrame                = [xmlReader spriteFrameAtIndex: i];
+        stickerName                 = [xmlReader sprinteNameAtIndex: i];
+        if ( CGRectEqualToRect( stickerFrame,  CGRectZero ) == true )
+        {
+            continue;
+        }
+        
+        stickerNewFrame             = calculateNewRectByScaled( stickerFrame, scale );
+        if ( CGRectContainsPoint( stickerNewFrame,  touchPoint ) == false )
+        {
+            continue;
+        }
+        
+        
+        //  set touch origin(OnScreen).
+        stickerNewFrame.origin      = [self convertPoint: stickerNewFrame.origin toView: nil];
+        
+        
+        //  ... delegate.
+        NSLog( @"touch point %s in rect %s", [NSStringFromCGPoint( touchPoint ) UTF8String], [NSStringFromCGRect( stickerFrame ) UTF8String] );
+        NSLog( @"sticker name : %s", [stickerName UTF8String] );
+        
+        return;
+    }
+    
+    
+    
+    
+    return;
+}
+
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+- ( CGFloat ) _CalculateScaleOfSize
+{
+    if ( ( [self backgroundView] == nil ) || ( nil == xmlReader ) )
+    {
+        return 0.0f;
+    }
+    
+    CGSize                          nowSize;
+    CGSize                          originalSize;
+    
+    nowSize                         = [[self backgroundView] bounds].size;
+    originalSize                    = [xmlReader textureSize];
+    if ( ( CGSizeEqualToSize( nowSize, CGSizeZero ) == true ) || ( CGSizeEqualToSize( originalSize, CGSizeZero ) == true ) )
+    {
+        return 0.0f;
+    }
+        
+    //  don't use height to calculate, because when image view setClipsToBounds at tab page, will change image height.
+    return ( originalSize.width / nowSize.width );
+}
+
 
 //  ------------------------------------------------------------------------------------------------
 //  ------------------------------------------------------------------------------------------------
@@ -118,6 +257,8 @@
     }
     
     [self                           _InitAttributes];
+    
+    [self                           _CreateTapAction];
     return self;
 }
 
@@ -131,14 +272,36 @@
     }
     
     [self                           _InitAttributes];
+    
+    [self                           _CreateTapAction];
     return self;
 }
 
 //  ------------------------------------------------------------------------------------------------
 - ( void ) dealloc
 {
+    if ( nil != framesData )
+    {
+        SAFE_ARC_ASSIGN_POINTER_NIL( framesData );
+    }
+    
+    if ( nil != xmlReader )
+    {
+        SAFE_ARC_RELEASE( xmlReader );
+        SAFE_ARC_ASSIGN_POINTER_NIL( xmlReader );
+    }
 
     SAFE_ARC_SUPER_DEALLOC();
+}
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+- ( BOOL ) loadFrames:(NSData *)confgureData
+{
+    framesData                      = confgureData;
+    
+    //  don't load & parse on here, delay to touch action.
+    return YES;
 }
 
 //  ------------------------------------------------------------------------------------------------
