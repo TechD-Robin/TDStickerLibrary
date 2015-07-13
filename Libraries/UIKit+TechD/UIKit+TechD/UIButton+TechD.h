da99d1f8a0808e84a5432d83802c94decf8d602c
Robin Hsu <robinhsu599+dev@gmail.com>
2015-07-14 02:29:29 +0800

    update library : UIKit+TechD.

--- Libraries/UIKit+TechD/UIKit+TechD/UIButton+TechD.h
+++ Libraries/UIKit+TechD/UIKit+TechD/UIButton+TechD.h
@@ -16,11 +16,74 @@
 @interface UIButton (TechD)
 
 //  ------------------------------------------------------------------------------------------------
+#pragma mark declare for create the object.
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief create a button object with image of normal and highlighted state.
+ *  create a button object with image of normal and highlighted state.
+ *
+ *  @param image                    normal image.
+ *  @param highlighted              highlighted image.
+ *  @param offset                   offset of origin position.
+ *
+ *  @return object|nil              the button object or nil.
+ */
 + ( instancetype ) buttonWithImage:(UIImage *)image highlighted:(UIImage *)highlighted origin:(CGPoint)offset;
 
 //  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief create a button object with image of several states.
+ *
+ *  @param image                    normal image.
+ *  @param highlighted              highlighted image.
+ *  @param disabled                 disabled image.
+ *  @param selected                 selected image.
+ *  @param offset                   offset of origin position.
+ *
+ *  @return object|nil              the button object or nil.
+ */
 + ( instancetype ) buttonWithImage:(UIImage *)image highlighted:(UIImage *)highlighted
                           disabled:(UIImage *)disabled selected:(UIImage *)selected origin:(CGPoint)offset;
+
+//  ------------------------------------------------------------------------------------------------
+#pragma mark declare for change imageView's position.
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief change image view to title label's right side.
+ *  change image view to title label's right side, the method must has title's object , otherwise has not effect of change.
+ *  in this method has used sizeToFit, that will change object's initial size.
+ *
+ *  @param offsetX                  offset of X after position is changed. ( value > 0 is move to right, else is move to left )
+ *
+ *  @return YES|NO                  method success or failure.
+ */
+- ( BOOL ) changeImageToTitleRightSide:(CGFloat)offsetX;
+
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief enforce move image view to new position for horizontal alignment.
+ *  enforce move image view to new position for horizontal alignment,
+ *  this method will ignore the property UIControl's contentHorizontalAlignment for imageView.
+ *
+ *  @param imageAlignment           image's horizontal alignment. ( UI...Fill has no effect. )
+ *  @param offsetX                  offset of X after position is changed. ( value > 0 is move to right, else is move to left )
+ *
+ *  @return YES|NO                  method success or failure.
+ */
+- ( BOOL ) enforceMoveImageHorizontalAlignment:(UIControlContentHorizontalAlignment)imageAlignment offset:(CGFloat)offsetX;
+
+//  ------------------------------------------------------------------------------------------------
+/**
+ *  @brief enforce move image view to new position for vertical alignment.
+ *  enforce move image view to new position for vertical alignment,
+ *  this method will ignore the property UIControl's contentVerticalAlignment for imageView.
+ *
+ *  @param imageAlignment           image's vertical alignment. ( UI...Fill has no effect. )
+ *  @param offsetY                  offset of Y after position is changed. ( value > 0 is move to bottom, elase is move to top )
+ *
+ *  @return YES|NO                  method success or failure.
+ */
+- ( BOOL ) enforceMoveImageVerticalAlignment:(UIControlContentVerticalAlignment)imageAlignment offset:(CGFloat)offsetY;
 
 //  ------------------------------------------------------------------------------------------------
 
@@ -28,3 +91,9 @@
 
 //  ------------------------------------------------------------------------------------------------
 //  ------------------------------------------------------------------------------------------------
+
+
+
+
+
+
--- Libraries/UIKit+TechD/UIKit+TechD/UIButton+TechD.m
+++ Libraries/UIKit+TechD/UIKit+TechD/UIButton+TechD.m
@@ -18,10 +18,11 @@
 
 //  --------------------------------
 //  ------------------------------------------------------------------------------------------------
+#pragma mark method for create the object.
+//  ------------------------------------------------------------------------------------------------
 + ( instancetype ) buttonWithImage:(UIImage *)image highlighted:(UIImage *)highlighted origin:(CGPoint)offset
 {
     NSParameterAssert( nil != image );
-    NSParameterAssert( nil != highlighted );
     
     UIButton                      * button;
     CGRect                          buttonFrame;
@@ -30,8 +31,15 @@
     NSParameterAssert( nil != button );
     
     buttonFrame.origin              = offset;
-    buttonFrame.size.width          = MAX( [image size].width, [highlighted size].width );
-    buttonFrame.size.height         = MAX( [image size].height, [highlighted size].height );
+    
+    buttonFrame.size.width          = [image size].width;
+    buttonFrame.size.height         = [image size].height;
+    if ( nil != highlighted )
+    {
+        buttonFrame.size.width      = MAX( [image size].width, [highlighted size].width );
+        buttonFrame.size.height     = MAX( [image size].height, [highlighted size].height );
+    }
+    
     [button                         setFrame: buttonFrame];
     [button                         setImage: image forState: UIControlStateNormal];
     [button                         setImage: highlighted forState: UIControlStateHighlighted];
@@ -69,6 +77,157 @@
         [button                     setImage: selected forState: UIControlStateSelected];
     }
     return button;
+}
+
+//  ------------------------------------------------------------------------------------------------
+#pragma mark method for change imageView's position.
+//  ------------------------------------------------------------------------------------------------
+- ( BOOL ) changeImageToTitleRightSide:(CGFloat)offsetX
+{
+    if ( [self imageView] == nil )
+    {
+        return NO;
+    }
+    
+    CGFloat                         titleWidth;
+    CGFloat                         imageWidth;
+    CGFloat                         gapWidth;
+    UIEdgeInsets                    titleEdgeInsets;
+    UIEdgeInsets                    imageEdgeInsets;
+    
+    [self                           sizeToFit];
+    titleWidth                      = ( ( [self titleLabel] != nil ) ? [[self titleLabel] frame].size.width : 0.0f );
+    imageWidth                      = [[self imageView] frame].size.width;
+    gapWidth                        = ( [self frame].size.width - ( titleWidth + imageWidth ) );
+    
+    titleEdgeInsets                 = ( ( [self titleLabel] != nil ) ? [self titleEdgeInsets] : UIEdgeInsetsZero );
+    imageEdgeInsets                 = [self imageEdgeInsets];
+    
+    titleEdgeInsets.left            -= imageWidth;
+    titleEdgeInsets.right           = ( imageWidth - titleEdgeInsets.right );
+    
+    imageEdgeInsets.left            += ( titleWidth + gapWidth + offsetX );
+    imageEdgeInsets.right           -= ( titleWidth + gapWidth + offsetX );
+    
+    [self                           setTitleEdgeInsets: titleEdgeInsets];
+    [self                           setImageEdgeInsets: imageEdgeInsets];
+    return YES;
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( BOOL ) enforceMoveImageHorizontalAlignment:(UIControlContentHorizontalAlignment)imageAlignment offset:(CGFloat)offsetX
+{
+    if ( [self imageView] == nil )
+    {
+        return NO;
+    }
+    
+    //  has no effect's alignment.
+    if ( UIControlContentHorizontalAlignmentFill == imageAlignment )
+    {
+        return NO;
+    }
+    
+    CGRect                          imageRect;
+    CGPoint                         imageCenter;
+    CGFloat                         calculateOffset;
+    UIEdgeInsets                    newImageEdgeInsets;
+    
+    calculateOffset                 = 0.0f;
+    imageCenter                     = [[self imageView] center];
+    imageRect                       = [[self imageView] frame];
+    newImageEdgeInsets              = [self imageEdgeInsets];
+    
+    //  first, calculate offset value for center.
+    calculateOffset                 = ( ( [self bounds].size.width / 2.0f ) - imageCenter.x );
+    
+    //  second, calculate offset value for object's alignment.
+    switch ( imageAlignment )
+    {
+        case UIControlContentHorizontalAlignmentCenter:
+        {
+            newImageEdgeInsets.left     = ( calculateOffset + offsetX );
+            newImageEdgeInsets.right    = -( calculateOffset + offsetX );
+            break;
+        }
+        case UIControlContentHorizontalAlignmentLeft:
+        {
+            calculateOffset             -= ( ( [self bounds].size.width - imageRect.size.width ) / 2.0f );
+            newImageEdgeInsets.left     += ( calculateOffset + offsetX );
+            newImageEdgeInsets.right    += -( calculateOffset + offsetX );
+            break;
+        }
+        case UIControlContentHorizontalAlignmentRight:
+        {
+            calculateOffset             += ( ( [self bounds].size.width - imageRect.size.width ) / 2.0f );
+            newImageEdgeInsets.left     += ( calculateOffset + offsetX );
+            newImageEdgeInsets.right    += -( calculateOffset + offsetX );
+            break;
+        }
+        default:
+            break;
+    }
+    
+    [self                           setImageEdgeInsets: newImageEdgeInsets];
+    return YES;
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( BOOL ) enforceMoveImageVerticalAlignment:(UIControlContentVerticalAlignment)imageAlignment offset:(CGFloat)offsetY
+{
+    if ( [self imageView] == nil )
+    {
+        return NO;
+    }
+    
+    //  has no effect's alignment.
+    if ( UIControlContentVerticalAlignmentFill == imageAlignment )
+    {
+        return NO;
+    }
+    
+    CGRect                          imageRect;
+    CGPoint                         imageCenter;
+    CGFloat                         calculateOffset;
+    UIEdgeInsets                    newImageEdgeInsets;
+    
+    calculateOffset                 = 0.0f;
+    imageCenter                     = [[self imageView] center];
+    imageRect                       = [[self imageView] frame];
+    newImageEdgeInsets              = [self imageEdgeInsets];
+    
+    //  first, calculate offset value for center.
+    calculateOffset                 = ( ( [self bounds].size.height / 2.0f ) - imageCenter.y );
+    
+    //  second, calculate offset value for object's alignment.
+    switch ( imageAlignment )
+    {
+        case UIControlContentVerticalAlignmentCenter:
+        {
+            newImageEdgeInsets.top      = ( calculateOffset + offsetY );
+            newImageEdgeInsets.bottom   = -( calculateOffset + offsetY );
+            break;
+        }
+        case UIControlContentVerticalAlignmentTop:
+        {
+            calculateOffset             -= ( ( [self bounds].size.height - imageRect.size.height ) / 2.0f );
+            newImageEdgeInsets.top      += ( calculateOffset + offsetY );
+            newImageEdgeInsets.bottom   += -( calculateOffset + offsetY );
+            break;
+        }
+        case UIControlContentVerticalAlignmentBottom:
+        {
+            calculateOffset             += ( ( [self bounds].size.height - imageRect.size.height ) / 2.0f );
+            newImageEdgeInsets.top      += ( calculateOffset + offsetY );
+            newImageEdgeInsets.bottom   += -( calculateOffset + offsetY );
+            break;
+        }
+        default:
+            break;
+    }
+
+    [self                           setImageEdgeInsets: newImageEdgeInsets];
+    return YES;
 }
 
 //  ------------------------------------------------------------------------------------------------
